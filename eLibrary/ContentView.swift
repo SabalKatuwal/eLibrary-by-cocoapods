e00dc17c3324ca820552e449aba7fa3a4520de67
SabalKatuwal <sabal.katuwal1@gmail.com>
2022-09-10 10:58:57 +0545

    first push nothing special

--- eLibrary/ContentView.swift
+++ eLibrary/ContentView.swift
@@ -9,8 +9,16 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack {
+            Text("Hello, earth!")
+                .padding()
+            Text("yo")
+                .foregroundColor(.yellow)
+  
+        }
+        
+            
+            
     }
 }
 
