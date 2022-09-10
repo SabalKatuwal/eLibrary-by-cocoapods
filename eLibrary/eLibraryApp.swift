f1576bd864729b742ce1f823efea3f0b4667a6df
SabalKatuwal <sabal.katuwal1@gmail.com>
2022-09-10 10:34:05 +0545

    Initial Commit

---
+++ eLibrary/eLibraryApp.swift
@@ -0,0 +1,17 @@
+//
+//  eLibraryApp.swift
+//  eLibrary
+//
+//  Created by Sabal on 9/10/22.
+//
+
+import SwiftUI
+
+@main
+struct eLibraryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
