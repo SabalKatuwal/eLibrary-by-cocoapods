9c32d7c9dde76d6a9b90d3e35b93fee6b9f2dcdb
SabalKatuwal <sabal.katuwal1@gmail.com>
2022-09-11 13:49:58 +0545

    this is done by using coocapods so i will do it again by using siwft package manager

--- eLibrary/eLibraryApp.swift
+++ eLibrary/eLibraryApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 @main
 struct eLibraryApp: App {
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
